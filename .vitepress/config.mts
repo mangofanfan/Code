802c59e9b96c5f4553211a6c761c66a9f0b2cdde
MangoFanFan_ <mangofanfanw@icloud.com>
2025-12-08 22:41:31 +0800

    增加 sitemap

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // Import lightbox plugin
 import lightbox from "vitepress-plugin-lightbox"
 
 // 代码组图标
-import { groupIconMdPlugin, groupIconVitePlugin, localIconLoader } from 'vitepress-plugin-group-icons'
+import {groupIconMdPlugin, groupIconVitePlugin, localIconLoader} from 'vitepress-plugin-group-icons'
 
 // 页面历史
 import {
@@ -18,124 +18,129 @@ import {common, c, cpp, java, python, css, scss, javascript, typescript} from ".
 // https://vitepress.dev/reference/site-config
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  lang: "zh-CN",
-  title: "Code Space",
-  description: "多语言代码空间",
-  head: [
-    // 添加图标
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
-  ],
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '食用', link: '/common/start' },
-      { text: '语言', items:[
-          { text: 'C', link: '/c/features' },
-          { text: 'C++', link: '/cpp/features' },
-          { text: 'Java', link: '/java/features' },
-          { text: 'Python', link: '/python/features' },
-          { text: 'CSS', link: '/css/features' },
-          { text: 'SCSS', link: '/scss/features' },
-          { text: 'JavaScript', link: '/javascript/features' },
-          { text: 'TypeScript', link: '/typescript/features' },
-        ]
-      },
+    lang: "zh-CN",
+    title: "Code Space",
+    description: "多语言代码空间",
+    head: [
+      // 添加图标
+      ['link', {rel: 'icon', href: '/favicon.ico'}]
     ],
+    themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      nav: [
+        {text: '食用', link: '/common/start'},
+        {
+          text: '语言', items: [
+            {text: 'C', link: '/c/features'},
+            {text: 'C++', link: '/cpp/features'},
+            {text: 'Java', link: '/java/features'},
+            {text: 'Python', link: '/python/features'},
+            {text: 'CSS', link: '/css/features'},
+            {text: 'SCSS', link: '/scss/features'},
+            {text: 'JavaScript', link: '/javascript/features'},
+            {text: 'TypeScript', link: '/typescript/features'},
+          ]
+        },
+      ],
 
-    sidebar: {
-      "/": common,
-      "/c/": c,
-      "/cpp/": cpp,
-      "/java/": java,
-      "/python/": python,
-      "/css/": css,
-      "/scss/": scss,
-      "/javascript/": javascript,
-      "/typescript/": typescript,
-    },
+      sidebar: {
+        "/": common,
+        "/c/": c,
+        "/cpp/": cpp,
+        "/java/": java,
+        "/python/": python,
+        "/css/": css,
+        "/scss/": scss,
+        "/javascript/": javascript,
+        "/typescript/": typescript,
+      },
 
-    outline: {
-      level: [2,4], // 显示2-4级标题
-      label: '在本页中: ' // 文字显示
-    },
+      outline: {
+        level: [2, 4], // 显示2-4级标题
+        label: '在本页中: ' // 文字显示
+      },
 
-    returnToTopLabel: "返回顶部",
+      returnToTopLabel: "返回顶部",
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/mangofanfan/Code' }
-    ],
+      socialLinks: [
+        {icon: 'github', link: 'https://github.com/mangofanfan/Code'}
+      ],
 
-    search: {
-      provider: 'algolia',
-      options: {
-        appId: '64M9HSA94L',
-        apiKey: '16e484639f0be9ad4f7f5e95dee714bb',
-        indexName: 'crawler_code-space'
-      }
-    },
+      search: {
+        provider: 'algolia',
+        options: {
+          appId: '64M9HSA94L',
+          apiKey: '16e484639f0be9ad4f7f5e95dee714bb',
+          indexName: 'crawler_code-space'
+        }
+      },
 
-    footer: {
-      message: 'License: GPL-v3. 文本协议 CC-BY',
-      copyright: '芒果帆帆 - 帆域网络 - 备案 <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2024110906号</a>',
-    },
+      footer: {
+        message: 'License: GPL-v3. 文本协议 CC-BY',
+        copyright: '芒果帆帆 - 帆域网络 - 备案 <a href="https://beian.miit.gov.cn/" target="_blank">苏ICP备2024110906号</a>',
+      },
 
-    lastUpdated: {
-      text: '最后更新于',
-      formatOptions: {
-        dateStyle: 'short', // 可选值full、long、medium、short
-        timeStyle: 'medium' // 可选值full、long、medium、short
+      lastUpdated: {
+        text: '最后更新于',
+        formatOptions: {
+          dateStyle: 'short', // 可选值full、long、medium、short
+          timeStyle: 'medium' // 可选值full、long、medium、short
+        },
+      },
+
+      editLink: {
+        pattern: 'https://github.com/mangofanfan/Code/edit/master/:path', // 改成自己的仓库
+        text: '在GitHub编辑本页'
       },
     },
 
-    editLink: {
-      pattern: 'https://github.com/mangofanfan/Code/edit/master/:path', // 改成自己的仓库
-      text: '在GitHub编辑本页'
-    },
-  },
-
-  markdown: {
-    config: (md) => {
-      // Use lightbox plugin
-      md.use(lightbox, {});
-      md.use(groupIconMdPlugin) //代码组图标
-    },
-    lineNumbers: true,
-  },
-
-  vite: {
-    // 阅读增强
-    optimizeDeps: {
-      exclude: [
-        '@nolebase/vitepress-plugin-enhanced-readabilities/client',
-        'vitepress',
-        '@nolebase/ui',
-      ],
-    },
-    ssr: {
-      noExternal: [
-        // 如果还有别的依赖需要添加的话，并排填写和配置到这里即可
-        '@nolebase/vitepress-plugin-enhanced-readabilities',
-        '@nolebase/ui',
-      ],
+    sitemap: {
+      hostname: "https://code.mangofanfan.cn"
     },
 
-    plugins: [
-      groupIconVitePlugin(
+    markdown: {
+      config: (md) => {
+        // Use lightbox plugin
+        md.use(lightbox, {});
+        md.use(groupIconMdPlugin) //代码组图标
+      },
+      lineNumbers: true,
+    },
+
+    vite: {
+      // 阅读增强
+      optimizeDeps: {
+        exclude: [
+          '@nolebase/vitepress-plugin-enhanced-readabilities/client',
+          'vitepress',
+          '@nolebase/ui',
+        ],
+      },
+      ssr: {
+        noExternal: [
+          // 如果还有别的依赖需要添加的话，并排填写和配置到这里即可
+          '@nolebase/vitepress-plugin-enhanced-readabilities',
+          '@nolebase/ui',
+        ],
+      },
+
+      plugins: [
+        groupIconVitePlugin(
           {
             customIcon: {
-                python: localIconLoader(import.meta.url, '../public/svg/python-icon.svg'),
-                c: localIconLoader(import.meta.url, '../public/svg/c-icon.svg'),
-                h: localIconLoader(import.meta.url, '../public/svg/c-icon.svg'),
-                cpp: localIconLoader(import.meta.url, '../public/svg/c++-icon.svg'),
-                cs: localIconLoader(import.meta.url, '../public/svg/c#-icon.svg'),
-                java: localIconLoader(import.meta.url, '../public/svg/java-icon.svg'),
-                js: 'logos:javascript', //js图标
-                md: 'logos:markdown', //markdown图标
-                css: 'logos:css-3', //css图标
+              python: localIconLoader(import.meta.url, '../public/svg/python-icon.svg'),
+              c: localIconLoader(import.meta.url, '../public/svg/c-icon.svg'),
+              h: localIconLoader(import.meta.url, '../public/svg/c-icon.svg'),
+              cpp: localIconLoader(import.meta.url, '../public/svg/c++-icon.svg'),
+              cs: localIconLoader(import.meta.url, '../public/svg/c#-icon.svg'),
+              java: localIconLoader(import.meta.url, '../public/svg/java-icon.svg'),
+              js: 'logos:javascript', //js图标
+              md: 'logos:markdown', //markdown图标
+              css: 'logos:css-3', //css图标
             },
           }
-      ),
-      GitChangelog({
+        ),
+        GitChangelog({
           repoURL: () => 'https://github.com/mangofanfan/Code',
           mapAuthors: [
             {
@@ -145,8 +150,9 @@ export default defineConfig({
               mapByNameAliases: ["MangoFanFan", "MangoFanFan_", "MangoFanFanw"]
             }
           ]
-      }),
-      GitChangelogMarkdownSection(),
-    ],
-  },
-})
+        }),
+        GitChangelogMarkdownSection(),
+      ],
+    },
+  }
+)
--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -1,183 +1,186 @@
 interface pageSidebarItem {
-    text: string,
-    items: {text: string, link: string}[]
+  text: string,
+  items: { text: string, link: string }[]
 }
 
 export const common = [
-    {
-        text: "关于本站",
-        items: [
-            {text: "如何食用", link: '/common/start'},
-            {text: "贡献", link: '/common/contribute'},
-            {text: "提问的艺术", link: '/common/how-to-question'},
+  {
+    text: "关于本站",
+    items: [
+      {text: "如何食用", link: '/common/start'},
+      {text: "贡献", link: '/common/contribute'},
+      {text: "提问的艺术", link: '/common/how-to-question'},
+    ]
+  },
+  {
+    text: "编程概念",
+    items: [
+      {text: "IDE", link: '/common/ide'},
+      {text: "面向对象", link: '/common/object-oriented'}
+    ]
+  },
+  {
+    text: "编码规范",
+    items: [
+      {text: "编码建议", link: '/common/advice'}
+    ]
+  },
+  {
+    text: "其他帮助",
+    items: [
+      {
+        text: "软件", items: [
+          {text: "软件", link: "/common/software"},
+          {text: "VSCode", link: '/common/vscode'},
+          {text: "JetBrains IDEs", link: '/common/jetbrains-ides'},
+          {text: "Git", link: '/common/git'},
+          {text: "WSL", link: '/common/wsl'},
+          {text: "CMake", link: '/common/cmake'},
+          {text: "vim", link: '/common/vim'},
+          {text: "杂项", link: '/common/others'},
         ]
-    },
-    {
-        text: "编程概念",
-        items: [
-            {text: "IDE", link: '/common/ide'},
-            {text: "面向对象", link: '/common/object-oriented'}
-        ]
-    },
-    {
-        text: "编码规范",
-        items: [
-            {text: "编码建议", link: '/common/advice'}
-        ]
-    },
-    {
-        text: "其他帮助",
-        items: [
-            {text: "软件", link: '/common/software', items: [
-                {text: "VSCode", link: '/common/vscode'},
-                {text: "JetBrains IDEs", link: '/common/jetbrains-ides'},
-                {text: "Git", link: '/common/git'},
-                {text: "WSL", link: '/common/wsl'},
-                {text: "CMake", link: '/common/cmake'},
-                {text: "vim", link: '/common/vim'},
-                {text: "杂项", link: '/common/others'},
-            ]},
-            {text: "Mono 字体", link: '/common/mono-fonts'},
-        ]
-    },
+      },
+      {text: "Mono 字体", link: '/common/mono-fonts'},
+    ]
+  },
 ]
 
 export const c = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/c/features'},
-            {text: "安装", link: '/c/install'},
-            {text: "执行", link: '/c/run'}
-        ]
-    } as pageSidebarItem,
-    {
-        text: "单文件入门",
-        items: [
-            {text: "数组", link: '/c/array'},
-            {text: "字符串", link: '/c/string'},
-            {text: "函数", link: '/c/function'},
-        ]
-    } as pageSidebarItem,
-    {
-        text: "多文件工程",
-        items: [
-            {text: "模块化与头文件", link: '/c/head-file'},
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/c/features'},
+      {text: "安装", link: '/c/install'},
+      {text: "执行", link: '/c/run'}
+    ]
+  } as pageSidebarItem,
+  {
+    text: "单文件入门",
+    items: [
+      {text: "数组", link: '/c/array'},
+      {text: "字符串", link: '/c/string'},
+      {text: "函数", link: '/c/function'},
+    ]
+  } as pageSidebarItem,
+  {
+    text: "多文件工程",
+    items: [
+      {text: "模块化与头文件", link: '/c/head-file'},
+    ]
+  } as pageSidebarItem
 ]
 
 export const cpp = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/cpp/features'},
-            {text: "安装（C）", link: '/c/install'},
-            {text: "执行（C）", link: '/c/run'}
-        ]
-    } as pageSidebarItem,
-    {
-        text: "基本",
-        items: [
-            {text: "输入输出与命名空间", link: '/cpp/cin-cout'},
-            {text: "函数", link: '/cpp/function'},
-        ]
-    } as pageSidebarItem,
-    {
-        text: "面向对象",
-        items: [
-            {text: "类型与实例", link: '/cpp/class-and-instantiation'}
-        ]
-    } as pageSidebarItem,
-    {
-        text: "多文件工程",
-        items: [
-            {text: "头文件", link: '/cpp/head-file'},
-            {text: "与C一同使用", link: '/cpp/work-with-c'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/cpp/features'},
+      {text: "安装（C）", link: '/c/install'},
+      {text: "执行（C）", link: '/c/run'}
+    ]
+  } as pageSidebarItem,
+  {
+    text: "基本",
+    items: [
+      {text: "输入输出与命名空间", link: '/cpp/cin-cout'},
+      {text: "函数", link: '/cpp/function'},
+    ]
+  } as pageSidebarItem,
+  {
+    text: "面向对象",
+    items: [
+      {text: "类型与实例", link: '/cpp/class-and-instantiation'}
+    ]
+  } as pageSidebarItem,
+  {
+    text: "多文件工程",
+    items: [
+      {text: "头文件", link: '/cpp/head-file'},
+      {text: "与C一同使用", link: '/cpp/work-with-c'}
+    ]
+  } as pageSidebarItem
 ]
 
 export const java = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/java/features'},
-            {text: "安装", link: '/java/install'},
-        ]
-    } as pageSidebarItem,
-    {
-        text: "基本",
-        items: [
-            {text: "类与方法", link: '/java/class-function'},
-        ]
-    } as pageSidebarItem,
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/java/features'},
+      {text: "安装", link: '/java/install'},
+    ]
+  } as pageSidebarItem,
+  {
+    text: "基本",
+    items: [
+      {text: "类与方法", link: '/java/class-function'},
+    ]
+  } as pageSidebarItem,
 ]
 
 export const python = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/python/features'},
-            {text: "碎碎念", link: '/python/something'},
-            {text: "安装与执行", link: '/python/install-run'},
-        ]
-    } as pageSidebarItem,
-    {
-        text: "入门",
-        items: [
-            {text: "基本类型", link: '/python/basic-type'},
-            {text: "循环", link: '/python/loop'},
-            {text: "条件与异常", link: "/python/if-try"},
-            {text: "函数", link: '/python/function'},
-            {text: "类型提示", link: '/python/type-hint'},
-            {text: "模块和包", link: '/python/module-and-package'},
-        ]
-    } as pageSidebarItem,
-    {
-        text: "面向对象",
-        items: [
-            {text: "类型与实例", link: '/python/class-and-instantiation'},
-            {text: "类型方法", link: '/python/class-method'},
-            {text: "特殊方法", link: '/python/special-method'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/python/features'},
+      {text: "碎碎念", link: '/python/something'},
+      {text: "安装与执行", link: '/python/install-run'},
+    ]
+  } as pageSidebarItem,
+  {
+    text: "入门",
+    items: [
+      {text: "基本类型", link: '/python/basic-type'},
+      {text: "循环", link: '/python/loop'},
+      {text: "条件与异常", link: "/python/if-try"},
+      {text: "函数", link: '/python/function'},
+      {text: "类型提示", link: '/python/type-hint'},
+      {text: "模块和包", link: '/python/module-and-package'},
+    ]
+  } as pageSidebarItem,
+  {
+    text: "面向对象",
+    items: [
+      {text: "类型与实例", link: '/python/class-and-instantiation'},
+      {text: "类型方法", link: '/python/class-method'},
+      {text: "特殊方法", link: '/python/special-method'}
+    ]
+  } as pageSidebarItem
 ]
 
 export const css = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/css/features'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/css/features'}
+    ]
+  } as pageSidebarItem
 ]
 
 export const scss = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/scss/features'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/scss/features'}
+    ]
+  } as pageSidebarItem
 ]
 
 export const javascript = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/javascript/features'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/javascript/features'}
+    ]
+  } as pageSidebarItem
 ]
 
 
 export const typescript = [
-    {
-        text: "概览",
-        items: [
-            {text: "语言特性", link: '/typescript/features'}
-        ]
-    } as pageSidebarItem
+  {
+    text: "概览",
+    items: [
+      {text: "语言特性", link: '/typescript/features'}
+    ]
+  } as pageSidebarItem
 ]
 
