0474e20bd32092e49edccdeb7c1be38684714bcb
MangoFanFan_ <mangofanfanw@icloud.com>
2025-12-09 23:18:30 +0800

    修复代码配置错误 和 主题色

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,16 +1,17 @@
-<!--===========================================================================
-  = Code Space 是位于 GitHub 上的开源项目，发起者为 MangoFanFan。                            =
-  = 项目部署在 code.mangofanfan.cn，遵循 CC BY / GPL 3.0 协议。                          =
-  ===========================================================================-->
+<!--=========================================================================-->
+<!-- Code Space 是位于 GitHub 上的开源项目，发起者为 MangoFanFan。 -->
+<!-- 项目部署在 code.mangofanfan.cn，遵循 CC BY / GPL 3.0 协议。 -->
+<!--=========================================================================-->
 
-<script setup>
+<script setup lang="ts">
 import DefaultTheme from "vitepress/theme";
-import { onMounted } from "vue";
-import { useRouter } from "vitepress";
+import { nextTick, onMounted, provide } from "vue";
+import { useData, useRouter } from "vitepress";
 import mediumZoom from "medium-zoom";
 
 const { Layout } = DefaultTheme;
 const router = useRouter();
+const { isDark } = useData();
 
 // 阅读增强
 import {
@@ -30,7 +31,7 @@ const setupMediumZoom = () => {
 // Apply medium zoom on load
 onMounted(async () => {
   // 导入 初始化字体 方法
-  const {initFontFamily} = await import("./font");
+  const {initFontFamily} = await import("./font.js");
   setupMediumZoom();
   initFontFamily();
 });
@@ -38,6 +39,40 @@ onMounted(async () => {
 // Subscribe to route changes to re-apply medium zoom effect
 router.onAfterRouteChange = setupMediumZoom;
 
+// 应用主题切换动画
+const enableTransitions = () =>
+    'startViewTransition' in document &&
+    window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+
+provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value
+    return
+  }
+
+  const clipPath = [
+    `circle(0px at ${x}px ${y}px)`,
+    `circle(${Math.hypot(
+        Math.max(x, innerWidth - x),
+        Math.max(y, innerHeight - y)
+    )}px at ${x}px ${y}px)`
+  ]
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value
+    await nextTick()
+  }).ready
+
+  document.documentElement.animate(
+      { clipPath: isDark.value ? clipPath.reverse() : clipPath },
+      {
+        duration: 300,
+        easing: 'ease-in',
+        fill: 'forwards',
+        pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+      }
+  )
+})
 </script>
 
 <template>
@@ -55,6 +90,8 @@ router.onAfterRouteChange = setupMediumZoom;
 </template>
 
 <style>
+@import "style/var.css";
+
 .medium-zoom-overlay {
   backdrop-filter: blur(5rem);
 }
@@ -62,5 +99,30 @@ router.onAfterRouteChange = setupMediumZoom;
 .medium-zoom-overlay,
 .medium-zoom-image--opened {
   z-index: 999;
+}
+
+/* 应用切换主题动画 */
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+
+.VPSwitchAppearance {
+  width: 22px !important;
+}
+
+.VPSwitchAppearance .check {
+  transform: none !important;
 }
 </style>
--- .vitepress/theme/index.mts
+++ .vitepress/theme/index.mts
@@ -1,4 +1,9 @@
-import { inBrowser } from "vitepress";
+//==============================================================================
+// Code Space 是位于 GitHub 上的开源项目，发起者为 MangoFanFan。
+// 项目部署在 code.mangofanfan.cn，遵循 CC BY / GPL 3.0 协议。
+//==============================================================================
+
+import {inBrowser, Theme} from "vitepress";
 import 'virtual:group-icons.css' //代码组样式
 import './style/index.css'
 
@@ -11,13 +16,11 @@ import "@chinese-fonts/lxgwwenkaibright/dist/LXGWBright-Italic/result.css"
 import "@chinese-fonts/lywkpmydb/dist/LXGWWenKaiScreen/result.css"  // 霞鹜文楷+屏幕阅读 * 2
 import "@chinese-fonts/lywkpmydb/dist/LXGWWenKaiMonoScreen/result.css"
 import "@chinese-fonts/dyh/dist/SmileySans-Oblique/result.css"  // 得意黑
-
-import "jetbrains-mono"
-
-import "misans/lib/Normal/MiSans-Bold.min.css"
+import "jetbrains-mono"  // JetBrains Mono
+import "misans/lib/Normal/MiSans-Bold.min.css"  // MiSans * 2
 import "misans/lib/Normal/MiSans-Normal.min.css"
 
-import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css'  // Element Plus
 
 // 页面历史
 import {
@@ -31,11 +34,12 @@ import WhatsClass from "./components/WhatsClass.vue"
 import FontsComboWidget from "./components/FontsComboWidget.vue";
 
 import Layout from "./Layout.vue";
+import {App} from "@vue/runtime-core";
 
 // noinspection JSUnusedGlobalSymbols
 export default{
     Layout,
-    enhanceApp({ app }) {// 仅在浏览器环境下加载 Element Plus
+    enhanceApp({ app }: {app: App}) {// 仅在浏览器环境下加载 Element Plus
         if (inBrowser) {
             // 动态导入 Element Plus，防止构建时报错
             import('element-plus').then((module) => {
@@ -46,4 +50,4 @@ export default{
         app.component('WhatsClass', WhatsClass);
         app.component('FontsComboWidget', FontsComboWidget);
     },
-} satisfies ThemeConfig;
+} satisfies Theme;
